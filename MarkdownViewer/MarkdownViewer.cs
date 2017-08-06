e55b04a537d6941f072fd6d4e3c50e18a37ffb3c
wangzhenfeng <zhfeng.wang@gmail.com>
2017-08-06 22:14:05 +0800

    initial commit

---
+++ MarkdownViewer/MarkdownViewer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Collections;
+using OY.TotalCommander.TcPluginInterface;
+using OY.TotalCommander.TcPluginInterface.Lister;
+using System.IO;
+
+namespace MarkdownViewer
+{
+    public class MarkdownViewer : ListerPlugin
+    {
+
+        public const String AllowedExtensions = ".md,.markdown";
+
+        public MarkdownViewer(StringDictionary pluginSettings) : base(pluginSettings)
+        {
+
+            if (String.IsNullOrEmpty(Title))
+            {
+                Title = "Markdown Viewer";
+            }
+
+            DetectString = "EXT=\"MD\"";
+
+        }
+
+        private ArrayList controls = new ArrayList();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileToLoad"></param>
+        /// <param name="showFlags"></param>
+        /// <returns></returns>
+        public override object Load(string fileToLoad, ShowFlags showFlags)
+        {
+            ViewerControl viewerControl = null;
+            if (!String.IsNullOrEmpty(fileToLoad))
+            {
+
+                String ext = Path.GetExtension(fileToLoad);
+                // 如果文件扩展名不在支持之列则直接返回
+                if (AllowedExtensions.IndexOf(ext, StringComparison.InvariantCultureIgnoreCase) < 0)
+                {
+                    return null;
+                }
+
+                viewerControl = new ViewerControl();
+                viewerControl.FileLoad(fileToLoad);
+
+                controls.Add(viewerControl);
+            }
+
+            return viewerControl;
+        }
+    }
+
+}
---
+++ MarkdownViewer/ViewerControl.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Markdig;
+
+namespace MarkdownViewer
+{
+    public partial class ViewerControl : UserControl
+    {
+
+        private Encoding encoding = Encoding.UTF8;
+        private const String CONTAINER_HTML = "<!DOCTYPE html>" +
+            "<html>" +
+            "<head>" +
+            "  <meta charset=\"utf-8\">" +
+            "  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />" +
+            "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\">" + 
+            "  <style type=\"text/css\">code{{white-space: pre;}}</style>" +
+            "  <style type=\"text/css\">" +
+            "   {1}" +
+            "  </style>" +
+            "</head>" + 
+            "<body>" +
+            "<article class=\"markdown-body\">" + 
+            "{0}" +
+            "</article>" + 
+            "</body>" + 
+            "</html>";
+
+
+        public ViewerControl()
+        {
+            InitializeComponent();
+        }
+
+       
+        public void FileLoad(String fileName)
+        {
+            using (StreamReader sr = new StreamReader(fileName, encoding))
+            {
+                String markdownContent = sr.ReadToEnd();
+                var pipline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+                String markdownHTML = Markdown.ToHtml(markdownContent, pipline);
+                String style = Properties.Resources.github_markdown2;
+                String html = String.Format(CONTAINER_HTML, markdownHTML, style);
+                this.webBrowser1.DocumentText = html;
+            }
+        }
+    }
+}
