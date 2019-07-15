0dec5be07f04c26a3971a6067ee2c5c4490484fb
wangzhenfeng <zhfeng.wang@gmail.com>
2019-07-15 09:37:33 +0800

    feature: support print file, select and copy

--- MarkdownViewer/MarkdownViewer.cs
+++ MarkdownViewer/MarkdownViewer.cs
@@ -3,6 +3,8 @@ using System.Collections.Specialized;
 using System.Collections;
 using OY.TotalCommander.TcPluginInterface.Lister;
 using System.IO;
+using System.Windows.Forms;
+using System.Drawing.Printing;
 
 namespace MarkdownViewer
 {
@@ -51,12 +53,12 @@ namespace MarkdownViewer
                 viewerControl = new ViewerControl(this);
                 viewerControl.FileLoad(fileToLoad);
                 FocusedControl = viewerControl.webBrowser1;
-                viewerControl.Focus();
+                //viewerControl.Focus();
 
                 controls.Add(viewerControl);
              
             }
-
+            
             return viewerControl;
         }
 
@@ -69,7 +71,54 @@ namespace MarkdownViewer
         {
             controls.Remove(control);
         }
-      
+
+        /// <summary>
+        /// Is called when click print button
+        /// </summary>
+        /// <param name="control"></param>
+        /// <param name="fileToPrint"></param>
+        /// <param name="defPrinter"></param>
+        /// <param name="printFlags"></param>
+        /// <param name="margins"></param>
+        /// <returns></returns>
+        public override ListerResult Print(object control, string fileToPrint, string defPrinter, PrintFlags printFlags, PrintMargins margins)
+        {
+
+            TraceProc(System.Diagnostics.TraceLevel.Info, "Print fileToPrint:" + fileToPrint + ", defPrinter: " + defPrinter);
+            ViewerControl viewerControl = (ViewerControl)control;
+            viewerControl.webBrowser1.ShowPrintDialog();
+            return ListerResult.OK;
+        }
+
+        public override ListerResult SendCommand(object control, ListerCommand command, ShowFlags parameter)
+        {
+            ViewerControl viewerControl = (ViewerControl)control;
+            switch (command)
+            {
+                case ListerCommand.Copy:
+                    viewerControl.webBrowser1.Document.ExecCommand("Copy", false, null);
+                    break;
+                case ListerCommand.SelectAll:
+                    viewerControl.webBrowser1.Document.ExecCommand("SelectAll", false, null);
+                    break;
+            }
+            return ListerResult.OK;
+        }
+
+
+        public override ListerResult SearchText(object control, string searchString, SearchParameter searchParameter)
+        {
+            ViewerControl viewerControl = (ViewerControl)control;
+            
+            return ListerResult.OK;
+        }
+
+        public override int NotificationReceived(object control, int message, int wParam, int lParam)
+        {
+            ViewerControl viewerControl = (ViewerControl)control;
+            return 0;
+        }
+
     }
 
 }
